4e357728d9d1b71da5c1800777867887bc6b032e
Angela Ordonez <angelitaooo@me.com>
2017-06-18 18:08:33 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  author = 'angela';
+  count = 0;
+  imgSource = './../favicon.ico';
+  enable = false;
 }
